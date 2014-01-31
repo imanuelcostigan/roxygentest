01b8ee5bec827efab12cff8cb917f12d7014d0e2
Imanuel Costigan <i.costigan@me.com>
2014-02-01 10:10:11 +1100

    Illustrate blob error. Roxygen issue 180

--- R/roxygen_test.R
+++ R/roxygen_test.R
@@ -1,32 +1,16 @@
-#' Class \code{"MyClass"}
+draw.Blob <- function (type = 'small') 
+{
+  'Something about blobs'
+  type <<- 'small'
+}
+
+#' Blob
+#'
+#' Blah blah blah
 #' 
-#' A class to illustrate roxygen bugs
-#' 
-#' @slot slot1 a numeric vector
-#' @slot slot2 a numeric vector
 #' @export
-
-setClass('MyClass', slots=c(slot1='numeric', slot2='numeric'))
-
-#' @aliases +,MyClass,numeric-method
-#' @rdname MyClass-class
-#' @export
-setMethod (
-  f = "+", 
-  signature = signature(e1 = "MyClass", e2 = "numeric"), 
-  definition = function (e1, e2)
-  {
-    e1@slot1 + e2@slot2
-  }
-)
-
-#' @aliases as.character,MyClass-method
-#' @rdname MyClass-class
-#' @export
-setMethod (
-  f = 'as.character', 
-  signature = 'MyClass',
-  definition = function (x, ...) {
-    as.character(x)
-  }
+Blob <- setRefClass (
+  Class = "Blob", 
+  fields = list(type = 'character'),
+  methods = list(draw = draw.Blob)
 )
